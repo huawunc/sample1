18702b81069084d29f5459e1b849842892b24994
huawunc <huawunc@gmail.com>
2019-08-30 08:15:11 +0000

    333

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -2,6 +2,9 @@ package com.leszko.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main Spring Application.
+ */
 
 @SpringBootApplication
 public class CalculatorApplication {
